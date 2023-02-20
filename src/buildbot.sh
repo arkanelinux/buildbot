88e99cce18407cf071aaeea051d1fe182bc09832
Dennis ten Hoove <dennistenhoove@protonmail.com>
2023-02-20 04:10:27 +0100

    Bug fixes and additional config option

--- src/buildbot.sh
+++ src/buildbot.sh
@@ -63,13 +63,13 @@ main () {
 
 								# Ensure dependencies are installed
 								source ./PKGBUILD
-								sudo pacman -Sy --noconfirm --needed ${makedepends}
+								sudo pacman -Sy --noconfirm --needed ${depends} ${makedepends}
 
 								# Import pgp keys if any
 								import_pgp_keys
 
 								# Build the package
-								makepkg -d
+								makepkg ${makepkg_params}
 								error_check $?
 							fi
 						fi
@@ -103,13 +103,13 @@ main () {
 
 				# Ensure dependencies are installed
 				source ./PKGBUILD
-				sudo pacman -Sy --noconfirm --needed ${makedepends}
+				sudo pacman -Sy --noconfirm --needed ${depends} ${makedepends}
 
 				# Import pgp keys if any
 				import_pgp_keys
 
 				# Build the package
-				makepkg -d
+				makepkg ${makepkg_params}
 				error_check $?
 			fi
 
